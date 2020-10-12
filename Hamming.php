543d7eb3f56ddbed26078d1fadbec10ad0103bad
Cristian Quintero García <cristian181199@gmail.com>
2020-10-12 19:48:01 +0200

    LOS 7 TEST PASADOS PASANDO LAS CADENAS A ARRAYS Y COMPARANDOLAS. (AÑADIDO EXPLICACIÓN DE LO QUE HACE EL PROGRAMA, FALTA LOS DERECHOS DE AUTOR)

--- Hamming.php
+++ Hamming.php
@@ -9,4 +9,16 @@ function distancia($a, $b)
     if (mb_strlen($a) != mb_strlen($b)) {
         return false;
     }
+    return count(array_diff_assoc(mb_str_split($a), mb_str_split($b)));
 }
+/*
+ * Programa que mide la distancia hamming de dos cadenas.
+ * 
+ * Con "mb_strlen" obtenemos la longitud de una cadena de caracteres y con la estructura "if" comprobamos si tienen distinta longitud con el "!="
+ * en el caso que tubieran distinta longitud, retornamos false.
+ * 
+ * Con "mb_str_split" pasamos de cadena a array de cada uno de sus caracteres y "array_diff_assoc" calcula la diferencia entre arrays con un
+ * chequeo adicional de índices.
+ * 
+ * Finalemnte con el count, contamos la diferencia entre arrays y la retornamos.
+ */
